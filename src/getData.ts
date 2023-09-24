5773aa50ff94aea39ae716062bc24a3d32e99691
Taha Ceken <taha-ceken@hotmail.com>
2023-09-24 14:41:09 +0300

    api try connect

--- src/getData.ts
+++ src/getData.ts
@@ -15,7 +15,7 @@ export async function fetchData(): Promise<any> {
 }
 
 
-// TODO ask about api fetch
+// // TODO ask about api fetch
 
 // const appKey: string = "your_app_key"; // Uygulama Kodu (app_key)
 // const domain: string = "your_domain"; // Alan Adı (domain)
@@ -41,3 +41,41 @@ export async function fetchData(): Promise<any> {
 //   .catch((error: Error) => {
 //     console.error("API isteği hata:", error);
 //   });
+
+
+import axios from 'axios';
+
+// Replace these with your actual values
+const app_key = 'your_unique_app_key';
+const domain = 'your_unique_domain';
+
+// Define the API URL
+const api_url = 'http://gl-apps.unlemcloud.com/api/applications';
+
+// Create headers with the required authentication parameters
+const headers = {
+  app_key,
+  domain,
+};
+
+// Function to fetch data from the API
+async function fetchData2(): Promise<void> {
+  try {
+    const response = await axios.get(api_url, { headers });
+
+    if (response.status === 200) {
+      const data = response.data;
+      // Now 'data' contains the API response data, and you can process it as needed
+      console.log(data);
+    } else {
+      // Handle errors here, e.g., log an error message or throw an exception
+      console.error(`Error: ${response.status} - ${response.statusText}`);
+    }
+  } catch (error: any) {
+    // Handle network errors or other exceptions
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+// Call the function to fetch data
+fetchData2();
